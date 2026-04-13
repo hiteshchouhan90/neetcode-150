2e395cbb14ae4df22a5071663c6d5de2fdd1f789
Hitesh Chouhan <hiteshchouhan90@users.noreply.github.com>
2026-04-13 13:26:12 +0100

    Remove: duplicate-integer - submission-1

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        value_list :list = []
-        for num in nums:
-            if num in value_list:
-                return True
-            else:
-                value_list.append(num)
-        return False
